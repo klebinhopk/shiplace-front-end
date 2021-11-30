7b913d4540ab4cc825ff63c080f6bbd9e3965519
Kleber Pereira <klebinho.pk@hotmail.com>
2021-11-30 17:44:43 -0300

    alteração crossorigin crossOrigin

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => (
       <title>shiplace - Best shipping rates</title>
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
     </Head>
 
